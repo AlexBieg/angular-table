0d33c2d226e7e16f9b463df51137d46e72513af4
AlexBieg <alexbieg95@gmail.com>
2015-11-02 17:11:33 -0800

    replaced with correct files for class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // Create an angular application called "sortApp"
-
+var sortApp = angular.module('sortApp', []);
 
 // Define a controller "mainController" for you application, 
 // making sure to pass in both the $scope and $http variables
+sortApp.controller('controller', function($scope, $http) {
+    $http.get('data/salaries.json').then(function(response) {
+        $scope.data = response.data
+    })
+});
 
 	// Use and $http.get request to get the salary data
 	
